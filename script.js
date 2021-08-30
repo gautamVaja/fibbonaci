bfe1533dcbb8119f7c8ed8213268e2b5b939a29a
Gautam Vaja <83920497+gautamVaja@users.noreply.github.com>
2021-08-30 16:43:22 +0530

    full project ready

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//https://clevelanddesign.com/wp-content/uploads/2020/09/the-golden-ratio-teaser.jpg
+$("#button").click(function (e) {
+  e.preventDefault();
+  var n = $("#n").val();
+  if (n == 0) {
+    return;
+  }
+  let x = 0;
+  let a = 0;
+  let b = 1;
+  if (n == 1) {
+    $("#ans").html("0");
+  } else if (n == 2) {
+    $("#ans").html("0 1");
+  } else {
+    var ans = "0 1 ";
+    for (let i = 2; i < n; i++) {
+      x = a + b;
+      ans += x + " ";
+      a = b;
+      b = x;
+    }
+    $("#ans").html(ans);
+  }
+});
